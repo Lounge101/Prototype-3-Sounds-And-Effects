92da7d799e4c767278c76d327fa3eeba6e2bda26
Lounge101 <noahtoledo170@riversideunified.org>
2023-10-18 13:32:05 -0700

    Left off at 3.2.4

--- Prototype 3/Assets/Scripts/Player Controller.cs
+++ Prototype 3/Assets/Scripts/Player Controller.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float JumpForce = 10f;
     public float GravityModifier = 1f;
+    public bool IsOnGround = true;
+    public bool GameOver = false;
 
     private Rigidbody _playerRb;
 
@@ -20,9 +22,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && IsOnGround)
         {
-            _playerRb.AddForce(Vector3.up * 100, ForceMode.Impulse);
+            _playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            IsOnGround = false;
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            IsOnGround = true;
+        }
+    }
+           
 }
